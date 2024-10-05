44c300cd38213070bec24b45e24cd235aa882773
Jheicer <jheicer.ro@gmail.com>
2024-10-04 22:12:18 -0500

    Implementación de consumo de servicio con WebClien

--- src/main/java/pe/edu/cibertec/Patitas_FrontEnd_wc/Config/WebClientConfig.java
+++ src/main/java/pe/edu/cibertec/Patitas_FrontEnd_wc/Config/WebClientConfig.java
@@ -1,15 +1,32 @@
 package pe.edu.cibertec.Patitas_FrontEnd_wc.Config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebClientConfig {
 
+    HttpClient httpClient = HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000) // Timeout para la conexión
+            .responseTimeout(Duration.ofSeconds(10)) // Timeout para la respuesta
+            .doOnConnected(conn ->
+                    conn.addHandlerLast(new ReadTimeoutHandler(10, TimeUnit.SECONDS)) // Timeout de lectura
+            );
+
     @Bean
     public WebClient webClientAuthentication(WebClient.Builder builder) {
+
         return builder
+                .baseUrl("http://localhost:8081/autentication")
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
 }
--- src/main/java/pe/edu/cibertec/Patitas_FrontEnd_wc/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/Patitas_FrontEnd_wc/controller/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 import pe.edu.cibertec.Patitas_FrontEnd_wc.DTO.LoginRequestDTO;
 import pe.edu.cibertec.Patitas_FrontEnd_wc.DTO.LoginResponseDTO;
 import pe.edu.cibertec.Patitas_FrontEnd_wc.ViewModel.LoginModel;
+import reactor.core.publisher.Mono;
 
 
 @Controller
@@ -45,18 +46,58 @@ public class LoginController {
 
         }
 
-        LoginRequestDTO loginRequestDTO = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
 
         try {
-            // Realizar la solicitud con WebClient
+
+            // Invocar servicio de autenticación
+            //String endpoint = "http://localhost:8090/autenticacion/login";
+            LoginRequestDTO loginRequestDTO = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
+            //LoginResponseDTO loginResponseDTO = webClientAuthentication.postForObject("/login", loginRequestDTO, LoginResponseDTO.class);
+
+            Mono<LoginResponseDTO> monoLoginResponseDTO = webClientAuthentication.post()
+                    .uri("/inicio")
+                    .body(Mono.just(loginRequestDTO), LoginRequestDTO.class)
+                    .retrieve()
+                    .bodyToMono(LoginResponseDTO.class);
+
+            // Bloquear la respuesta para obtener el resultado bloqueante o sincronico
+            LoginResponseDTO loginResponseDTO = monoLoginResponseDTO.block();
+
+            if (loginResponseDTO.codigo().equals("00")){
+
+                LoginModel loginModel = new LoginModel("00", "", loginResponseDTO.nombreUsuario());
+                model.addAttribute("loginModel", loginModel);
+                return "principal";
+
+            } else {
+
+                LoginModel loginModel = new LoginModel("02", "Error: Autenticación fallida", "");
+                model.addAttribute("loginModel", loginModel);
+                return "inicio";
+
+            }
+
+        } catch(Exception e) {
+
+            LoginModel loginModel = new LoginModel("99", "Error: Ocurrió un problema en la autenticación", "");
+            model.addAttribute("loginModel", loginModel);
+            System.out.println(e.getMessage());
+            return "inicio";
+
+        }
+
+
+        /*try {
+
+            LoginRequestDTO loginRequestDTO = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
+
             LoginResponseDTO response = webClientAuthentication
                     .post()
-                    .uri("/login")
+                    .uri("http://localhost:8081/vehiculo/inicio")
                     .bodyValue(loginRequestDTO)
                     .retrieve()
                     .bodyToMono(LoginResponseDTO.class)
                     .block();
-
 
             if (response != null && "00".equals(response.codigo())) {
                 model.addAttribute("loginModel", response);
@@ -68,7 +109,7 @@ public class LoginController {
         } catch (WebClientResponseException e) {
             model.addAttribute("loginModel", new LoginModel("99", "Error de conexión al servidor", ""));
             return "inicio";
-        }
+        }*/
 
     }
 
